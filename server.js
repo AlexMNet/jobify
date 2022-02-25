9b61ff063a7e16b27351e44755604101a78f8615
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-02-24 23:53:56 -0800

    server setup

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log('Server running...');
+import 'dotenv/config';
+import express from 'express';
+const app = express();
+
+import connectDB from './db/connect.js';
+
+//Middleware
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+//Unhandled route middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+//Only start server if connection to database is successful
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
